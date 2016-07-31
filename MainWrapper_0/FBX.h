2cd901dcb2602a49a63d9aebba149f64c2ff691d
Federico Barlotti <banderi002@gmail.com>
2016-07-31 23:14:08 +0200

    Added local FBXSDK library files for compilation

--- MainWrapper_0/FBX.h
+++ MainWrapper_0/FBX.h
@@ -3,9 +3,29 @@
 
 #include <vector>
 #include <Windows.h>
-#include <fbxsdk.h>
+#include "../FBXSDK/include/fbxsdk.h"
 
-#pragma comment (lib, "libfbxsdk.lib")
+#if _WIN32 || _WIN64
+#if _WIN64
+#define ENVIRONMENT64
+#else
+#define ENVIRONMENT32
+#endif
+#endif
+
+#ifdef ENVIRONMENT32
+#ifdef _DEBUG
+#pragma comment (lib, "../FBXSDK/lib/x86/libfbxsdk-mtd.lib")
+#else
+#pragma comment (lib, "../FBXSDK/lib/x86/libfbxsdk-mt.lib")
+#endif
+#elif defined(ENVIRONMENT64)
+#ifdef _DEBUG
+#pragma comment (lib, "../FBXSDK/lib/x64/libfbxsdk-mtd.lib")
+#else
+#pragma comment (lib, "../FBXSDK/lib/x64/libfbxsdk-mt.lib")
+#endif
+#endif
 
 struct MyVertex
 {
